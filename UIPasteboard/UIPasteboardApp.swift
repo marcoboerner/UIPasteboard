327c5398a34271979b45900fc3c4920b6834614c
Marco Boerner <mail@marcoboerner.com>
2022-02-09 19:33:36 +0100

    Initial Commit

---
+++ UIPasteboard/UIPasteboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPasteboardApp.swift
+//  UIPasteboard
+//
+//  Created by Marco Boerner on 09.02.22.
+//
+
+import SwiftUI
+
+@main
+struct UIPasteboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
